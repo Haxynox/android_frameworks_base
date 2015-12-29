1fc35eaa0a14a99f0a2705aed267fcd78e4302b9
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-12-29 01:55:26 +0100

    More various access patches (incl. for GAPPS support)
    PackageManagerService: Grant signature permission to setup and search (dhacker29)
    
    These packages get protectionLevel=2 due to PROTECTION_FLAG_APPOP
    
    Enforce weaker grant if app is default handler for miltiple action (Svetoslav)
    
    Fix to show inifinite resolveractivity (joonyoung.cho)
    
    if launcher app doesn't have a CATEGORY_DEFAULT,
    it always show resolveractivity for selecting launcher.
    because findPreferredActivity finds only CATEGORY_DEFAULT app.
    but getHomeActivities pass to all CATEGORY_HOME.
    
    Remove duplicate assignment statement (Richard Uhler)

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -3732,12 +3732,6 @@ public class PackageManagerService extends IPackageManager.Stub {
 
             PermissionsState permissionsState = sb.getPermissionsState();
 
-            // Only the package manager can change flags for system component permissions.
-            final int flags = permissionsState.getPermissionFlags(bp.name, userId);
-            if ((flags & PackageManager.FLAG_PERMISSION_SYSTEM_FIXED) != 0) {
-                return;
-            }
-
             boolean hadState = permissionsState.getRuntimePermissionState(name, userId) != null;
 
             if (permissionsState.updatePermissionFlags(bp, userId, flagMask, flagValues)) {
@@ -7116,7 +7110,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                                 for (int j=0; j<sysPs.pkg.libraryNames.size(); j++) {
                                     if (name.equals(sysPs.pkg.libraryNames.get(j))) {
                                         allowed = true;
-                                        allowed = true;
                                         break;
                                     }
                                 }
@@ -8697,6 +8690,10 @@ public class PackageManagerService extends IPackageManager.Stub {
                 // For development permissions, a development permission
                 // is granted only if it was already granted.
                 allowed = origPermissions.hasInstallPermission(perm);
+            }
+            if ((pkg.packageName.equals("com.google.android.katniss")) ||
+                    (pkg.packageName.equals("com.google.android.tungsten.setupwraith"))) {
+                allowed = true;
             }
         }
         return allowed;
@@ -14420,6 +14417,7 @@ public class PackageManagerService extends IPackageManager.Stub {
     public ComponentName getHomeActivities(List<ResolveInfo> allHomeCandidates) {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
 
         final int callingUserId = UserHandle.getCallingUserId();
         List<ResolveInfo> list = queryIntentActivities(intent, null,
